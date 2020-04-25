688681335886b590a757308a4364dfb1212550e7
Gabriel Morillo <gbrlmrllo@gmail.com>
2020-04-25 12:32:03 -0300

    remove credentials setup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = Rails.application.credentials.secret_key_base
+  # config.secret_key = Rails.application.credentials.secret_key_base
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -299,7 +299,7 @@ Devise.setup do |config|
 
   # ==> JWT Config
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.jwt_secret
+    jwt.secret = ENV["JWT_SECRET_KEY"]
     jwt.dispatch_requests = [
       ["POST", %r{^/login$}]
     ]
